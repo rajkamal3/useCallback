3b2ddfa7e3be509fb5aa72fb364ea05e2f686823
Raj Kamal Chenumalla <rkchenumalla@gmail.com>
2021-07-12 23:08:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import List from "./List";
 function App() {
   const [number, setNumber] = useState(1);
   const [dark, setDark] = useState(false);
-
+  
+  // useCallback will return a memoized version of the callback that only changes if one of the inputs has changed.
   const getItems = useCallback(() => {
     return [number, number + 1, number + 2];
   }, [number]);
